1ee83f1f406ebb8825fac81c74eb544215a0efa8
Vincent Esche <regexident@gmail.com>
2016-10-26 15:13:51 +0200

    Initial commit

---
+++ Blues/Blues.h
@@ -0,0 +1,19 @@
+//
+//  Blues.h
+//  Blues
+//
+//  Created by Vincent Esche on 26/10/2016.
+//  Copyright © 2016 NWTN Berlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Blues.
+FOUNDATION_EXPORT double BluesVersionNumber;
+
+//! Project version string for Blues.
+FOUNDATION_EXPORT const unsigned char BluesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Blues/PublicHeader.h>
+
+
---
+++ BluesTests/BluesTests.swift
@@ -0,0 +1,36 @@
+//
+//  BluesTests.swift
+//  BluesTests
+//
+//  Created by Vincent Esche on 26/10/2016.
+//  Copyright © 2016 NWTN Berlin. All rights reserved.
+//
+
+import XCTest
+@testable import Blues
+
+class BluesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
