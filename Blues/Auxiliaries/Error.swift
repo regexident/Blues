d281b636877cfad7831f88ba828accbc5476b26c
Michał Kałużny <maku@justmaku.org>
2018-10-30 18:25:55 +0100

    Use concrete error types instead of string messages.

---
+++ Blues/Auxiliaries/Error.swift
@@ -0,0 +1,39 @@
+//
+//  Error.swift
+//  Blues
+//
+//  Created by Vincent Esche on 31/10/2016.
+//  Copyright © 2016 NWTN Berlin. All rights reserved.
+//
+
+import Foundation
+
+/// Errors related to a descriptor.
+public enum TypedDescriptorError: Swift.Error {
+    /// The descriptor failed to encode its value.
+    case encodingFailed(error: Swift.Error)
+    /// The descriptor failed to decode its value.
+    case decodingFailed(error: Swift.Error)
+    /// The descriptor does not implement the given transform
+    ///
+    /// For example a read-only characteristic might choose to not implement the
+    /// encoding transform or a write-only the decoding transform respectively.
+    case transformNotImplemented
+    /// Another error has occured
+    case other(Swift.Error)
+}
+
+/// Errors related to a characteristic.
+public enum TypedCharacteristicError: Swift.Error {
+    /// The characteristic failed to encode its value.
+    case encodingFailed(error: Swift.Error)
+    /// The characteristic failed to decode its value.
+    case decodingFailed(error: Swift.Error)
+    /// The characteristic does not implement the given transform
+    ///
+    /// For example a read-only characteristic might choose to not implement the
+    /// encoding transform or a write-only the decoding transform respectively.
+    case transformNotImplemented
+    /// Another error has occured
+    case other(Swift.Error)
+}
